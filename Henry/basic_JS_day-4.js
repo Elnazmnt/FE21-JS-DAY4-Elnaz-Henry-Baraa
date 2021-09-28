1b32055cd44959e9090da60fb0111249bd8a3ba8
Henry <h.ngosytchev@gmail.com>
2021-09-28 14:51:05 +0200

    "Exercise 2: done"

--- Henry/basic_JS_day-4.js
+++ Henry/basic_JS_day-4.js
@@ -9,7 +9,6 @@ function extractData() {
     selector[1].innerHTML = lastName;
     selector[2].innerHTML = age;
 
-    //TO DO afte break!!!!
     if (lastName.length > 5) {
         selector[1].style.color = "green";
     } else {
@@ -21,3 +20,18 @@ document.getElementById("btn").addEventListener("click", extractData);
 
 
 //BASIC 2
+function extractJob() {
+    let selectedJob = document.getElementById("profession").value;
+    let Job = document.getElementById("profession");
+    Job.innerHTML = selectedJob;
+
+    if (selectedJob == "it") {
+        document.getElementById("div").style.background = "purple";
+    } else if (selecteddocument.getElementById("div") == "hospitality") {
+        document.getElementById("div").style.background = "yellow";
+    } else {
+        document.getElementById("div").style.background = "lightgrey";
+    }
+}
+
+document.getElementById("select_job").addEventListener("click", extractJob);
